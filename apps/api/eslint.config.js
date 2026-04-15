21ce5b69369bf2491f1c99797a093c3f8f7845ac
craftoxinz <winnestkyouka12@gmail.com>
2026-04-15 10:04:38 +0700

    chore(api): separate eslint config by file type and fix parser options

--- apps/api/eslint.config.js
+++ apps/api/eslint.config.js
@@ -5,27 +5,38 @@ import tseslint from 'typescript-eslint';
 import globals from 'globals';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default defineConfig({
-  files: ['**/*.{js,mjs,ts,mts}'],
-  extends: [
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-    eslintConfigPrettier,
-  ],
-  languageOptions: {
-    globals: {
-      ...globals.builtin,
-      ...globals.nodeBuiltin, // modern node esm
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    extends: [js.configs.recommended],
+  },
+  {
+    files: ['**/*.{ts,mts,cts}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    languageOptions: {
+      parserOptions: {
+        projectService: false,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
     },
   },
-  rules: {
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        args: 'all',
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
       },
-    ],
+    },
   },
-});
+  eslintConfigPrettier,
+]);
