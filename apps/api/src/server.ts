14630c48e527e01c1d073a8d91174fe2ecbb71a5
craftoxinz <winnestkyouka12@gmail.com>
2026-04-18 18:28:14 +0700

    refactor(api): remove unused variable from server

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,8 +1,6 @@
 import app from './app.ts';
 import { env } from './config/env.ts';
 
-let message;
-
 const startServer = () => {
   app.listen(env.PORT, () => {
     console.log(`Server listening on port ${env.PORT}`);
